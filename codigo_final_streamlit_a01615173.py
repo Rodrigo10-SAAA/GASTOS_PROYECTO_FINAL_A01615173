44711627a74341e35b47c38999dfbdf19a7f8228
Rodrigo10-SAAA <a01615173@tec.mx>
2025-12-01 16:31:42 -0600

    Update codigo_final_streamlit_a01615173.py

--- codigo_final_streamlit_a01615173.py
+++ codigo_final_streamlit_a01615173.py
@@ -34,7 +34,7 @@ y = datos["costo"]
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=01615173)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1615173)
 LR = LinearRegression()
 LR.fit(X_train,y_train)
 
